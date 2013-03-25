4e7e9119b36005b5e48fd856d6e1120d79aba2a9
xanti <bstalcup@nd.edu>
2013-03-25 15:03:38 +0100

    made the stage class

---
+++ stage.h
@@ -0,0 +1,22 @@
+/* FCII Final Project
+Spring 2013
+Michael Powers
+Brad Stalcup
+Sean Murphy
+Neal Sheehan
+Solara
+Unit Implementation file */
+
+#include "unit.h"
+
+
+
+class Stage{
+	Stage();
+	void perform();
+	int isInBounds(int, int);
+	void addArea(int, int, int, int);
+	void draw();
+	void addUnit(Unit);
+	void removeUnit(Unit)
+};
