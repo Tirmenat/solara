f3cd1df74e6cec3776e2d25234bce553dd270b91
smmurphy50 <smmurphy50@gmail.com>
2013-03-27 14:10:43 -0300

    Update Stage.cpp

--- Stage.cpp
+++ Stage.cpp
@@ -7,5 +7,13 @@ Stage::Stage()
 int isInBounds(){
   if(a>size_x || a<0 || b<size_y || b<0) return 0;
   else return 1;
+}
 
+void addUnit(Unit spr, double x, double y){
+  spr.setX(x);
+  spr.setY(y);
+}
+
+void removeUnit(Unit spr){
+  
 }
