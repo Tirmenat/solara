874bed2ca414162b92cc09edea4283048454c5f1
smmurphy50 <smmurphy50@gmail.com>
2013-04-15 20:55:30 -0300

    Update Hero.h

--- Hero.h
+++ Hero.h
@@ -19,11 +19,8 @@ class Hero: public Unit{
   Hero(double, double, double, double, double, double);
   virtual void draw();
   virtual void collide(Unit*);
-<<<<<<< HEAD
   virtual void increment();
-=======
-  void processEvent(SDL_Event event)
->>>>>>> 58beb1f9ad4d5a87b9ffcf8222cda1611729d07e
+  void processEvent()
 
  private:
  double x;
