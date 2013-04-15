58beb1f9ad4d5a87b9ffcf8222cda1611729d07e
smmurphy50 <smmurphy50@gmail.com>
2013-04-15 20:41:13 -0300

    Update Hero.cpp

--- Hero.cpp
+++ Hero.cpp
@@ -11,3 +11,45 @@ void Hero::draw()
 void Hero::collide(Unit* u)
 {
 }
+
+void Hero::processEvent(SDL_Event event)
+{
+  switch(event.type)
+  {
+    case SDL_KEYDOWN:
+      switch(event.key.keysym.sym)
+      { 
+        case SDLK_UP: 
+          ay=-ACCEL_Y;
+          break;
+        case SDLK_DOWN:
+          ay=ACCEL_Y;
+          break;
+        case SDLK_LEFT:
+          ax=-ACCEL_Y;
+          //Set sprite to left sprite
+          break;
+        case SDLK_RIGHT:
+          ax=ACCEL_X;
+          //Set sprite to right sprite
+          break;
+      }
+      break;
+    case SDL_KEYUP:
+      switch(event.key.keysym.sym)
+      {
+        case SDLK_UP:
+          ay=0;
+          break;
+        case SDLK_DOWN:
+          ay=0;
+          break;
+        case SDLK_LEFT:
+          ax=0;
+          break;
+        case SDLK_RIGHT:
+          ax=0;
+          break;
+      }
+  }
+}
