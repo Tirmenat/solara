a683850eaff32b03069de58c68e4200295f518f7
mike <mpowers3@nd.edu>
2013-04-19 13:40:12 -0400

    Updated the Unit/Enemy/main files, working on getting title screen integrated to the game like I had for last lab

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "SDL/SDL_image.h"
 #include "Stage.h"
 #include "Hero.h"
+#include "Enemy.h"
 
 using namespace std;
 
@@ -14,6 +15,12 @@ int main(void)
   stage_test.addArea(250,0,250,250);
   stage_test.addArea(150,150,250,250);
   stage_test.addArea(150,350,300,150);
+
+  Enemy enemy_test(100,100,0,0,0,0,20);
+  stage_test.addUnit(&enemy_test);
+
+  //Enemy enemy_test2(100,150,0,0,0,0,1);
+  //stage_test.addUnit(&enemy_test2);
 
   Hero hero_test(50,50,0,0,0,0,0);
   stage_test.addUnit(&hero_test);
@@ -33,7 +40,19 @@ int main(void)
 	    {
 	      quit = true;
 	    }
+	  if( event.type == SDL_KEYDOWN )
+	    {
+	      switch (event.key.keysym.sym)
+		{
+		case SDLK_q:
+		  quit=true;
+		}
+	    }
 	}
+      double herox = hero_test.getx();
+      double heroy = hero_test.gety();
+      enemy_test.chase(herox,heroy,dt);
+      //enemy_test2.chase(herox,heroy,dt);
       hero_test.processEvent(dt);
       stage_test.perform(dt);
       stage_test.draw();
