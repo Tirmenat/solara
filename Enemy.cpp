d3dde924ad5c3fa06a4e650817d3d2dcac2772c7
smmurphy50 <smurph17@nd.edu>
2013-04-24 16:10:00 -0400

    Fixed some constants I think. Also tried to fix the way the enemy moves.

--- Enemy.cpp
+++ Enemy.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-#define ACCEL_X_GO .005
-#define ACCEL_Y_GO .005
-#define ACCEL_X_STOP .001
-#define ACCEL_Y_STOP .001
+#define ACCEL_X_GO .05
+#define ACCEL_Y_GO .05
+#define ACCEL_X_STOP .01
+#define ACCEL_Y_STOP .01
 #define _USE_MATH_DEFINES
 
 Enemy::Enemy(double X, double Y, double VX, double VY, double AX, double AY, int location):Unit(X,Y,VX,VY,AX,AY,location)
@@ -27,23 +27,30 @@ void Enemy::draw()
 
 void Enemy::chase(double X, double Y, int dt)
 {
-  double angle = atan ((Y-y)/(X-x));
-  
+  double angle;
+  if (X==x){
+    if (Y>y) angle = 3.14159/2;
+  }
+  else if (Y==y){
+    if(X<x)angle = 0;
+  }
+  else{
+    angle = atan ((Y-y)/(X-x));
+  }
   if (Y>y) {
-    ay = ACCEL_Y_GO * sin(angle)*dt;
+    ay = ACCEL_Y_GO * sin(angle);
   }
 
   else {
-    ay = -ACCEL_Y_GO * sin(angle)*dt;
+    ay = -ACCEL_Y_GO * sin(angle);
   }
-  
 
   if (X>x) {
-    ax = ACCEL_X_GO * cos(angle)*dt;
+    ax = ACCEL_X_GO * cos(angle);
   }
 
   else {
-    ax = -ACCEL_X_GO * cos(angle)*dt;
+    ax = -ACCEL_X_GO * cos(angle);
   }
 
   
--- Unit.cpp
+++ Unit.cpp
@@ -18,8 +18,8 @@ Unit Implementation file */
 #define SPRWIDTH 16
 #define UNIT_LEFT 0
 #define UNIT_RIGHT 1
-#define MAX_VX 1
-#define MAX_VY 1
+#define MAX_VX .25
+#define MAX_VY .25
 
 Unit::Unit(){
   x=0;
