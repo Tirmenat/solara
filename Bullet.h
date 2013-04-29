8a821cecb617115ea8c4a8a78e637d6ca50dc12d
Neal W Sheehan <nsheeha1@mahler.helios.nd.edu>
2013-04-29 13:15:45 -0400

    Updated Bullet class with draw function

--- Bullet.h
+++ Bullet.h
@@ -9,6 +9,14 @@ Hero header file */
 
 #ifndef BULLET_H
 #define BULLET_H
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+#define BULLETV 600
+#define BULLETLENGTH 9
+#define BULLETWIDTH 9
+#define BULLETNUM 8
 
 #include "Unit.h"
 
@@ -16,10 +24,15 @@ class Bullet: public Unit{
  public:
   Bullet(Unit*,int,int);
   virtual void draw();
+  int getColor();
+  virtual bool isBullet();
  private:
-  int directionl;
-  int x;
-  int y;
+  int direction;
+  int red;
+  int green;
+  int blue;
+  SDL_Rect clip_bullet[BULLETNUM];
+  virtual void set_clips(int);
 }
 
 
