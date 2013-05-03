1454e607c91d41ad95e88d140f108607c9569120
mike <mpowers3@nd.edu>
2013-05-03 01:09:47 -0400

    Comments

--- Brad.cpp
+++ Brad.cpp
@@ -1,41 +1,41 @@
-#include "Brad.h"
+#include "Brad.h" //include header files and libraries
 #include "SDL/SDL.h"
 #include <cmath>
 #include "Hero.h"
 #include "Bullet.h"
 
-using namespace std;
+using namespace std; //use std namespace
 
-Brad::Brad(double XP1, double YP1, double XP2, double YP2,double maxv, int h, Stage* STAGE, Hero* hero):Enemy(XP1,YP1,maxv,6,h,hero)
+Brad::Brad(double XP1, double YP1, double XP2, double YP2,double maxv, int h, Stage* STAGE, Hero* hero):Enemy(XP1,YP1,maxv,6,h,hero) //constructor
 {
-  xp1=XP1;
+  xp1=XP1; //these are patrol points
   yp1=YP1;
   xp2=XP2;
   yp2=YP2;
-  moving=1;
+  moving=1; //control variables
   state=1;
-  stage=STAGE;
+  stage=STAGE; //pointer to stage
 }
 
-void Brad::chase(double herox, double heroy)
+void Brad::chase(double herox, double heroy) //chase function
 {
-  double angle;
+  double angle; //declare local variables
   double angle2;
   double v;
   double vx, vy;
   double x, y;
   double holdvx, holdvy;
-  x=getx();
+  x=getx(); //get values from enemy
   y=gety();
   v=getmaxv();
-  if(state==1 && moving==1){
-    holdx=herox;
+  if(state==1 && moving==1){ // at the beginning, get the location of the hero
+    holdx=herox; //store it in holdx and holdy
     holdy=heroy;
-    moving=2;
+    moving=2; //set moving to 2
   }
-  if(state==1 && moving==2){
-    angle = atan ((holdy-y)/(holdx-x));
-    if (x>holdx && y>holdy){
+  if(state==1 && moving==2){ //this is after we get location of hero
+    angle = atan ((holdy-y)/(holdx-x)); //calculate angle from point 1 to hero point
+    if (x>holdx && y>holdy){ //make velocites based on angle, maxv, and whichever quadrant we are in
       vy = -v * sin(angle);
       vx = -v * cos(angle);
     }
@@ -60,9 +60,9 @@ void Brad::chase(double herox, double heroy)
       vy = v * sin(angle);
     }
   }
-  else if(state==2){
-    angle=atan ((yp2-y)/(xp2-x));
-    if (x>xp2 && y>yp2){
+  else if(state==2){ //if were in state 2
+    angle=atan ((yp2-y)/(xp2-x)); //calculate the angle
+    if (x>xp2 && y>yp2){ //apply the velocities based on which quadrant we're in
       vy = -v * sin(angle);
       vx = -v * cos(angle);
     }
@@ -86,16 +86,16 @@ void Brad::chase(double herox, double heroy)
       vx = v * cos(angle);
       vy = v * sin(angle);
     }
-    moving=1;
+    moving=1; //set moving back to 1
   }
-  if(state==3 && moving==1){
-    holdx=herox;
+  if(state==3 && moving==1){ //if we're in state 3
+    holdx=herox; //store hero's  location in these holder variables
     holdy=heroy;
-    moving=2;
+    moving=2; //set moving to 2
   }
-  if(state==3 && moving==2){
-    angle = atan ((holdy-y)/(holdx-x));
-    if (x>holdx && y>holdy){
+  if(state==3 && moving==2){ //Move from location 2 towards wherever hero was
+    angle = atan ((holdy-y)/(holdx-x)); //calculate the angle
+    if (x>holdx && y>holdy){ //apply velocities based on whichever quadrant we are in
       vy = -v * sin(angle);
       vx = -v * cos(angle);
     }
@@ -120,9 +120,9 @@ void Brad::chase(double herox, double heroy)
       vy = v * sin(angle);
     }
   }
-  if(state==4){
-    angle=atan ((yp1-y)/(xp1-x));
-    if (x>xp1 && y>yp1){
+  if(state==4){ //if we're in state 4 send us back to point 1
+    angle=atan ((yp1-y)/(xp1-x)); //calculate angle
+    if (x>xp1 && y>yp1){ //apply velocities based on which quadrant we're in
       vy = -v * sin(angle);
       vx = -v * cos(angle);
     }
@@ -147,11 +147,11 @@ void Brad::chase(double herox, double heroy)
       vy = v * sin(angle);
     }
   }
-  if(sqrt((x-xp2)*(x-xp2)+(y-yp2)*(y-yp2)) <= 5){
+  if(sqrt((x-xp2)*(x-xp2)+(y-yp2)*(y-yp2)) <= 5){ //Moves us from state 2 to 3
     state=3;
     moving=1;
   }
-  if(sqrt((x-holdx)*(x-holdx)+(y-holdy)*(y-holdy)) <= 3) {
+  if(sqrt((x-holdx)*(x-holdx)+(y-holdy)*(y-holdy)) <= 3) { //If we reach a point where hero previously was, we shoot bullets towards him
     angle2=atan((heroy-y)/(herox-x));
     if(x>herox){
       angle2 = -angle2 + M_PI;
@@ -160,23 +160,22 @@ void Brad::chase(double herox, double heroy)
       angle2=-angle2;
     }
     Bullet *bullet=new Bullet(this,angle2,0,1,0,false);
-    bullet->setvx(bullet->getvx()-vx/2);
-    bullet->setvy(bullet->getvy()-vy/2);
+    bullet->setvx(bullet->getvx()-vx/2); //Make new bullets, dont account for
+    bullet->setvy(bullet->getvy()-vy/2); //brads velocity
     stage->addUnit(bullet);
-    cout<<"Add bullet"<<endl;
     //stage->addUnit(new Bullet(this,angle2,0,0,0,false));
     
-    if(state==1){
+    if(state==1){ //send us to next state based on previous state
       state=2;
     }
     else if(state==3){
       state=4;
     }
   }
-  if(sqrt((x-xp1)*(x-xp1)+(y-yp1)*(y-yp1)) <= 5){
+  if(sqrt((x-xp1)*(x-xp1)+(y-yp1)*(y-yp1)) <= 5){ //state 1 from 4
     state=1;
     moving=1;
   }
-  setvx(vx);
+  setvx(vx); //set the velocities
   setvy(vy);
 }
--- Brad.h
+++ Brad.h
@@ -7,29 +7,29 @@ Neal Sheehan
 Solara                                                                         
 Brad header file */
 
-#ifndef BRAD_H
+#ifndef BRAD_H //Preprocessing wrappers
 #define BRAD_H
 
-#include <iostream>
+#include <iostream> //Include header files and libraries
 #include <iomanip>
 #include "Enemy.h"
 #include "Hero.h"
 #include "Stage.h"
 
-class Brad: public Enemy{
- public:
-  Brad(double,double,double,double,double,int,Stage*,Hero*);
-  virtual void chase (double,double);
- private:
-  int state;
+class Brad: public Enemy{ //declaration of class
+ public: //public functions
+  Brad(double,double,double,double,double,int,Stage*,Hero*); //constructor
+  virtual void chase (double,double); //chase function
+ private: //private data members
+  int state; // state control variables
   int moving;
-  double holdx;
+  double holdx; //hold variables
   double holdy;
-  double xp1;
+  double xp1; //2 "patrol" points
   double yp1;
   double xp2;
   double yp2;
-  Stage* stage;
+  Stage* stage; //stage pointer
 };
 
 
